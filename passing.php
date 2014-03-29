91b32ba1937c76094f575c076704ba4fa9cdaa29
rwilcox5 <rwilcox5@gmail.com>
2014-03-29 11:19:00 -0400

    Update passing.php

--- passing.php
+++ passing.php
@@ -1,10 +1,56 @@
+
 <!DOCTYPE html>
 <html>
 <body>
 
-<?php
-echo "My first PHP script!";
-?>
+<script>
+
+var offensiveline = new Array();
+offensiveline[0] = [50,50,50,50,50];
+offensiveline[1] = [50,50,50,50,50];
+offensiveline[2] = [50,50,50,50,50];
+offensiveline[3] = [50,50,50,50,50];
+offensiveline[4] = [50,50,50,50,50];
+
+var receivers = new Array();
+receivers[0] = [50,50,50,50,50];
+receivers[1] = [50,50,50,50,50];
+receivers[2] = [50,50,50,50,50];
+
+var backs = new Array();
+backs[0] = [50,50,50,50,50];
+
+var tightends = new Array();
+tightends[0] = [50,50,50,50,50];
+
+var qb = new Array();
+qb[0] = [50,50,50,50,50];
+
+var defensiveline = new Array();
+defensiveline[0] = [50,50,50,50,50];
+defensiveline[1] = [50,50,50,50,50];
+defensiveline[2] = [50,50,50,50,50];
+defensiveline[3] = [50,50,50,50,50];
+
+var linebackers = new Array();
+linebackers[0] = [50,50,50,50,50];
+linebackers[1] = [50,50,50,50,50];
+linebackers[2] = [50,50,50,50,50];
+
+var defensivebacks = new Array();
+defensivebacks[0] = [50,50,50,50,50];
+defensivebacks[1] = [50,50,50,50,50];
+defensivebacks[2] = [50,50,50,50,50];
+defensivebacks[3] = [50,50,50,50,50];
+
+var stoprun = .5; //This is the defense's setting to stop the run
+var stoppass = 1-stoprun; //This is the defense's setting to stop the pass
+var isplayaction = 1; //This is 1 if the play is play-action, 0 if not
+var blitzers = [5]; //This is the defensive id of any blitzers.
+
+
+
+</script>
 
 </body>
 </html>
