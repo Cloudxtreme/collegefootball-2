ce5fd8f24cbe0e48fc250f660eacf4fb44ae000e
rwilcox5 <rwilcox5@gmail.com>
2014-03-29 21:38:46 -0400

    Update passing.js

--- passing.js
+++ passing.js
@@ -69,6 +69,9 @@ defplayer[8] = [2,defensivebacks[1]];
 defplayer[9] = [2,defensivebacks[2]];
 defplayer[10] = [2,defensivebacks[3]];
 
+var route = new Array();
+route = [0,0,0,0,0,0,1,2,3,0,0];
+
 var stoprun = .5; //This is the defense's setting to stop the run
 var stoppass = 1-stoprun; //This is the defense's setting to stop the pass
 var isplayaction = 1; //This is 1 if the play is play-action, 0 if not
@@ -82,6 +85,7 @@ var pressure = new Array();
 var openness = new Array();
 var sacklimit;
 var preslimit;
+
 function time(defplayerid,snaptime)
 {
 var offplayerid = defplayerid;
@@ -93,12 +97,87 @@ var press = snaptime*Math.pow(2,(((defplayer[defplayerid][1][0]-offplayer[offpla
 
 return press;
 }
-alert('do it');
+
+function open(offplayerid,snaptime)
+{
+var defplayerid = offplayerid;
+var openfactor = 60;
+var openrand = .25;
+var openpower = 1.1;
+var openness = Math.pow(snaptime,openpower)*Math.pow(2,(((offplayer[offplayerid][1][0]-defplayer[defplayerid][1][0])+(Math.random()-.5)*Math.pow(2,-(offplayer[offplayerid][1][3]-defplayer[defplayerid][1][3])*openrand))/openfactor));
+return openness;
+}
+function yardsout(offplayerid,snaptime)
+{
+route = route[offplayerid];
+switch(route)
+{
+case 0:
+	deepness = snaptime*offplayer[offplayerid][1][2]; \\time*speed
+case 1:
+if (snaptime<2)
+{
+	deepness = snaptime*offplayer[offplayerid][1][2];
+}
+else
+{
+	deepness = snaptime*offplayer[offplayerid][1][2]*.5;
+}
+case 2:
+if (snaptime<2)
+{
+	deepness = snaptime*offplayer[offplayerid][1][2];
+}
+else
+{
+	deepness = snaptime*offplayer[offplayerid][1][2]*.75;
+}
+case 3:
+if (snaptime<2)
+{
+	deepness = snaptime*offplayer[offplayerid][1][2];
+}
+else
+{
+	deepness = snaptime*offplayer[offplayerid][1][2]*.25;
+}
+}
+return deepness;
+}
+var receiverorder = new Array();
+receiverorder = [6,7,8];
+
+function qbdec(target,openlimit,deeplimit,snaptime)
+{
+var maxpres = 0;
+for (var i=0;i<4;i++)
+{
+pressure[i] = time(i,snaptime);
+if (pressure[i]>maxpres)
+{
+maxpres = pressure[i];
+}
+}
+if (maxpres>preslimit)
+{
+openlimit = openlimit*.75;
+}
+openlimit = 10*deeplimit/yardsout(target, snaptime)*openlimit;
+openness = open(target,snaptime);
+if (openness>openlimit)
+{
+throwto(target);
+}
+}
+
+qbdec(6,2.5,.5,1);
+
+/*
 var sumtime = 0;
 var ntime = 0;
 for (var ii=0;ii<100;ii++)
 {
-defensiveline[0] = [75,50,50,50,50];
+defensiveline[0] = [50,50,50,50,50];
 defplayer[0] = [0,defensiveline[0]];
 for (var i=0;i<150;i++)
 {
@@ -113,8 +192,29 @@ break;
 }
 alert(sumtime/ntime);
 
-alert('did it');
-//This determines how open a particular receiver is at a given time.
+var sumtime = 0;
+var ntime = 0;
+for (var ii=0;ii<100;ii++)
+{
+receivers[0] = [50,50,50,50,50];
+offplayer[0] = [0,receivers[0]];
+defensiveline[0] = [75,50,50,50,50];
+defplayer[0] = [0,defensiveline[0]];
+for (var i=0;i<150;i++)
+{
+openness = open(0,i/10.);
+if (openness>openlimit)
+{
+sumtime = sumtime+i/10;
+ntime = ntime+1;
+break;
+}
+}
+}
+alert(sumtime/ntime);
+*/
+
+
 
 </script>
 
